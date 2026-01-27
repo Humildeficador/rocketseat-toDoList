af581cf32c4111d9bf2bab9ecf1db8ae930d56da
Humildeficador <jvictor.leandro@gmail.com>
2026-01-27 13:49:48 -0300

    Container component c reateed

--- src/App.tsx
+++ src/App.tsx
@@ -9,52 +9,52 @@ import { Button } from "./components/Button/Button"
 import { ButtonIcon } from './components/ButtonIcon/ButtonIcon'
 import { Card } from './components/Card/Card'
 import { Checkbox } from './components/Checkbox/Checkbox'
+import { Container } from './components/Container/Container'
 import { Icon } from "./components/Icon/Icon"
 import { Input } from './components/Input/Input'
 import { Text } from "./components/Text/Text"
 
 export function App() {
 	return (
-		<div className="grid gap-10">
-			<div className="flex flex-col gap-2">
-				<Text variant={"body-md-bold"} className="text-pink-base">
-					Hello World!
-				</Text>
-				<Text variant={"body-sm"}>Hello World!</Text>
-				<Text variant={"body-sm-bold"}>Hello World!</Text>
+		<Container>
+			<div className="grid gap-10">
+				<div className="flex flex-col gap-2">
+					<Text variant={"body-md-bold"} className="text-pink-base">
+						Hello World!
+					</Text>
+					<Text variant={"body-sm"}>Hello World!</Text>
+					<Text variant={"body-sm-bold"}>Hello World!</Text>
+				</div>
+				<div className="flex gap-2">
+					<Icon svg={TrashIcon} className="fill-green-base" />
+					<Icon svg={CheckIcon} />
+					<Icon svg={PencilIcon} />
+					<Icon svg={PlusIcon} />
+					<Icon svg={XIcon} />
+					<Icon svg={SpinnerIcon} animate />
+				</div>
+				<div className='flex gap-2'>
+					<Badge variant={"secundary"}>5</Badge>
+					<Badge variant={"primary"}>2 de 5</Badge>
+				</div>
+				<div>
+					<Button icon={PlusIcon}>Nova Tarefa</Button>
+				</div>
+				<div className='flex gap-2'>
+					<ButtonIcon icon={TrashIcon}/>
+					<ButtonIcon variant={'secundary'} icon={TrashIcon}/>
+					<ButtonIcon variant={'terciary'} icon={TrashIcon}/>
+				</div>
+				<div>
+					<Input/>
+				</div>
+				<div>
+					<Checkbox  />
+				</div>
+				<div>
+					<Card size={'md'}>Olá Mundo</Card>
+				</div>
 			</div>
-
-			<div className="flex gap-2">
-				<Icon svg={TrashIcon} className="fill-green-base" />
-				<Icon svg={CheckIcon} />
-				<Icon svg={PencilIcon} />
-				<Icon svg={PlusIcon} />
-				<Icon svg={XIcon} />
-				<Icon svg={SpinnerIcon} animate />
-			</div>
-
-			<div className='flex gap-2'>
-				<Badge variant={"secundary"}>5</Badge>
-				<Badge variant={"primary"}>2 de 5</Badge>
-			</div>
-
-			<div>
-				<Button icon={PlusIcon}>Nova Tarefa</Button>
-			</div>
-			<div className='flex gap-2'>
-				<ButtonIcon icon={TrashIcon}/>
-				<ButtonIcon variant={'secundary'} icon={TrashIcon}/>
-				<ButtonIcon variant={'terciary'} icon={TrashIcon}/>
-			</div>
-			<div>
-				<Input/>
-			</div>
-			<div>
-				<Checkbox  />
-			</div>
-			<div>
-				<Card size={'md'}>Olá Mundo</Card>
-			</div>
-		</div>
+		</Container>
 	)
 }
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,38 @@
+import { cva, type VariantProps } from "class-variance-authority"
+import { type ComponentProps, createElement, type JSX } from "react"
+
+export const containerVariants = cva("mx-auto",
+  {
+    variants: {
+      size: {
+        md: "max-w-[31.5rem] px-2",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  },
+)
+
+interface ContainerProps
+  extends ComponentProps<"div">,
+    VariantProps<typeof containerVariants> {
+  as?: keyof JSX.IntrinsicElements
+}
+
+export function Container({
+  as = "div",
+  size,
+  className,
+  children,
+  ...props
+}: ContainerProps) {
+  return createElement(
+    as,
+    {
+      className: containerVariants({ size, className }),
+      ...props,
+    },
+    children,
+  )
+}
