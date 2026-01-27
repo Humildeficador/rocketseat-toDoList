7f49eb16280e856bedf0c758ab90fe4c4422a36b
Humildeficador <jvictor.leandro@gmail.com>
2026-01-27 15:11:52 -0300

    TaskSummary core component created

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,5 +1,10 @@
-import { Container } from '../components/Container/Container';
+import { Container } from "../components/Container/Container"
+import { TaskSummary } from '../core-components/TaskSummary/TaskSummary'
 
 export function PageHome() {
-  return <Container>Pagina Home!</Container>
+	return <Container as='article' className='space-y-3'>
+    <header className='flex justify-between'>
+      <TaskSummary />
+    </header>
+  </Container>
 }
