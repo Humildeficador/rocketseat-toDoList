e1d1871fac76571ff06dd3c8f0f6477f74bf7c43
Humildeficador <jvictor.leandro@gmail.com>
2026-01-27 13:50:59 -0300

    container cva variant size: max-w-[31.5rem] > max-w-126

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -5,7 +5,7 @@ export const containerVariants = cva("mx-auto",
   {
     variants: {
       size: {
-        md: "max-w-[31.5rem] px-2",
+        md: "max-w-126 px-2",
       },
     },
     defaultVariants: {
